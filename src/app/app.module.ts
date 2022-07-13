b57ff06494002af6362c2e1fe4b5267c6f309c10
sheapa <patshea26@gmail.com>
2022-07-13 16:26:53 -0400

    one way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './server/server.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessalertComponent } from './successalert/successalert.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServersComponent,
+    ServerComponent,
+    NavbarComponent,
+    WarningAlertComponent,
+    SuccessalertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  //selector: '[app-servers]',
+  selector: '.app-servers',
+  templateUrl: './servers.component.html',
+  //styleUrls: ['./servers.component.css']
+  styles: [`
+    h3 {
+      color: dodgerblue;
+    }
+  `]
+})
+export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
+  serverCreationStatus = 'No server was created!';
+  serverName: any = '';
+
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+   }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was Created!'
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+}
